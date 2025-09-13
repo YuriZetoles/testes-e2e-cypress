38b7dc7f3c79e410119ab653970840dcba91b451
Yuri Zetoles <yurizetoles0123@gmail.com>
2025-09-12 20:37:32 -0400

    feat: corrigir descrições e valores nos testes de jornada de usuário para transações e depósitos

--- cypress/e2e/jornada_inicio_transacao.cy.js
+++ cypress/e2e/jornada_inicio_transacao.cy.js
@@ -3,12 +3,12 @@
 
 /// <reference types="cypress" />
 
-describe('Jornada de usuário transações', () => {
+describe('página inicial', () => {
     beforeEach(() => {
         cy.visit('/')
     })
 
-    it('deve permitir que o usuário acesse a aplicação, realize uma transação e faça logout', () => {
+    it('deve permitir que o usuário acesse a aplicação, realize uma transação e faça um logout', () => {
         cy.login('neilton@alura.com', '123456')
         cy.url().should('eq', 'http://localhost:3000/home')
         cy.getByData('select-opcoes').select('Transferência')
@@ -19,28 +19,31 @@ describe('Jornada de usuário transações', () => {
         cy.location('pathname').should('eq', '/')
     })
 
-    it('deve permitir que o usuário acesse a aplicação, realize um depósito e faça logout', () => {
+    it('deve permitir que o usuário acesse a aplicação, realize um depósito e faça um logout', () => {
         cy.login('neilton@alura.com', '123456')
         cy.url().should('eq', 'http://localhost:3000/home')
         cy.getByData('select-opcoes').select('Depósito')
-        cy.getByData('form-input').type('100')
+        cy.getByData('form-input').type('50')
         cy.getByData('realiza-transacao').click()
-        cy.getByData('lista-transacoes').find('li').last().contains('+ R$ 100')
+        cy.getByData('lista-transacoes').find('li').last().contains('R$ 50')
         cy.getByData('botao-sair').click()
         cy.location('pathname').should('eq', '/')
     })
 
-    it('deve permitir que o usuario acesse a aplicacao, realize operações de transação e depósito, confirmando o saldo após cada operação', () => {
+    it('deve permitir que o usuário acesse a aplicação, realize operações de transação e depósito, confirmando o saldo após cada operação', () => {
         cy.login('neilton@alura.com', '123456')
         cy.url().should('eq', 'http://localhost:3000/home')
         cy.getByData('select-opcoes').select('Transferência')
         cy.getByData('form-input').type('100')
         cy.getByData('realiza-transacao').click()
         cy.getByData('lista-transacoes').find('li').last().contains('- R$ 100')
+        cy.get('[data-testid="saldo"]').should('be.visible').and('contain', 'R$')
         cy.getByData('select-opcoes').select('Depósito')
-        cy.getByData('form-input').type('100')
+        cy.getByData('form-input').clear().type('200')
         cy.getByData('realiza-transacao').click()
-        cy.getByData('lista-transacoes').find('li').last().contains('+ R$ 100')
+        cy.getByData('lista-transacoes').find('li').last().contains('R$ 200')
+        cy.get('[data-testid="saldo"]').should('be.visible').and('contain', 'R$')
+        cy.getByData('lista-transacoes').find('li').should('have.length.at.least', 2)
         cy.getByData('botao-sair').click()
         cy.location('pathname').should('eq', '/')
     })
